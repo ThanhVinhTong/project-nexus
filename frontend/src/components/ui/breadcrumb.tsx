067093e8969676ab344e510ccf80edb197f29884
Karl_Sue <tuankhanh17032003@gmail.com>
2025-09-21 23:57:26 +0800

    refactor: replace with add alias

--- frontend/src/components/ui/breadcrumb.tsx
+++ frontend/src/components/ui/breadcrumb.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { ChevronRightIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 
 function Breadcrumb({ ...props }: React.ComponentProps<"nav">) {
   return <nav aria-label="breadcrumb" data-slot="breadcrumb" {...props} />;
